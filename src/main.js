23b38f2bcefc072bb34c0d8c904195c85feeb6d9
wuyazhen <1036625283@qq.com>
2017-06-01 10:43:50 +0800

    用ajax配置vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import store from './store/index'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
@@ -13,6 +14,7 @@ const router = new VueRouter({
 	base:__dirname,
 	routes:[
 	    {path:'/',component:Index},
+        {path:'/index',component:Index},
 	    {path:'/creat',component:Creat}
 	]
 })
@@ -20,11 +22,19 @@ const router = new VueRouter({
 /* eslint-disable no-new */
 var vm = new Vue({
     router,
+    store,
     template:`
         <div>
             <transition name="fade" mode="out-in">
                 <router-view></router-view>
             </transition>
         </div>
-    `
+    `,
+    beforeCreate:function(){
+        if(this.$store.state.newbi == 1){
+            router.push({path:'/index'})
+        }else{
+            router.push({path:'/creat'})
+        }
+    }
 }).$mount("#app")
